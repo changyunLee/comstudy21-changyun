67a0613d22abbf376da5a2ccfe406be39e6e0e91
changyun <chang950317@naver.com>
2021-09-23 12:14:52 +0900

    파이어 베이스 파일

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqZFdfvLwYCrAzXEE3hPAiQMSy5MxUOb8",
+  authDomain: "changyun-9f622.firebaseapp.com",
+  databaseURL: "https://changyun-9f622-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "changyun-9f622",
+  storageBucket: "changyun-9f622.appspot.com",
+  messagingSenderId: "188172358938",
+  appId: "1:188172358938:web:c0cae6d248a43ec88e48a3",
+  measurementId: "G-9WX449943S"
+};
+
+firebase.initializeApp(firebaseConfig);
